c45fc95d050be196319628a3106503f76f9eca28
Greggory Garceau <ggarceau212@gmail.com>
2021-09-07 20:16:45 -0400

    Added input event listener to update image src

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+var $urlInput = document.querySelector('.photo-url');
+var $userPhoto = document.querySelector('.user-photo');
+
+$urlInput.addEventListener('input', function (event) {
+  var imageUrl = $urlInput.value;
+  $userPhoto.src = imageUrl;
+});
