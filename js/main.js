d9eb67fd665440721d2af0d3c520c6293bfed8e9
Greggory Garceau <ggarceau212@gmail.com>
2021-09-07 21:07:20 -0400

    Added submit event listener on journal entry form

--- js/main.js
+++ js/main.js
@@ -7,3 +7,22 @@ $urlInput.addEventListener('input', function (event) {
   var imageUrl = $urlInput.value;
   $userPhoto.src = imageUrl;
 });
+
+var $entryForm = document.querySelector('.entry-form');
+
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var $title = $entryForm.elements['picture-title'].value;
+  var $url = $entryForm.elements['photo-url'].value;
+  var $notes = $entryForm.elements.notes.value;
+  var entryForm = {
+    title: $title,
+    url: $url,
+    notes: $notes,
+    nextEntryId: data.nextEntryId
+  };
+  data.entries.push(entryForm);
+  data.nextEntryId++;
+  $userPhoto.src = 'images/placeholder-image-square.jpg';
+  $entryForm.reset();
+});
